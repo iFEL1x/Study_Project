29cec4911a719b42bd67b092829ef34807a277a8
FelixKengerli <ifelru@mail.ru>
2023-05-31 23:31:03 +0300

    Updated animation hero

--- Assets/PixelCrew/Hero.cs
+++ Assets/PixelCrew/Hero.cs
@@ -15,11 +15,17 @@ namespace PixelCrew
         private Rigidbody2D _rigidbody;
         private Vector2 _direction;
         private Animator _animator;
+        private SpriteRenderer _sprite;
+        
+        private static readonly int IsGroundKey = Animator.StringToHash("is-ground");
+        private static readonly int IsRunning = Animator.StringToHash("is-running");
+        private static readonly int VerticalVelocity = Animator.StringToHash("vertical-velocity");
     
         private void Awake()
         {
             _rigidbody = GetComponent<Rigidbody2D>();
             _animator = GetComponent<Animator>();
+            _sprite = GetComponent<SpriteRenderer>();
         }
     
         public void SetDirection(Vector2 direction)
@@ -46,7 +52,20 @@ namespace PixelCrew
                 _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, _rigidbody.velocity.y * 0.5f);
             }
             
-            _animator.SetBool("is-running", _direction.x != 0);
+            _animator.SetBool(IsGroundKey, isGrounded);
+            _animator.SetBool(IsRunning, _direction.x != 0);
+            _animator.SetFloat(VerticalVelocity, _rigidbody.velocity.y);
+
+            UpdateSpriteDirection();
+        }
+
+        private void UpdateSpriteDirection()
+        {
+            if (_direction.x > 0)
+                _sprite.flipX = false;
+            
+            else if (_direction.x < 0)
+                _sprite.flipX = true;
         }
     
         private bool IsGrounded()
